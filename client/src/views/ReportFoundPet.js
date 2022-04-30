4b09e52e62cf8ed249fce9b93f0c1142d0309e04
Nalini Mehendale <nalinimehendale@Nalinis-MacBook-Pro.local>
2022-04-30 14:44:54 -0700

    temp changes

--- client/src/views/ReportFoundPet.js
+++ client/src/views/ReportFoundPet.js
@@ -1,6 +1,22 @@
 function ReportFoundPet() {
     return (
-        "RECORD FOUND pet details"
+        <form>
+        <label>Enter pet name:
+            <input type="text" />  
+        </label>
+        <label>Enter pet color:
+            <input type="text" />  
+        </label>
+        <label>Enter pet breed:
+            <input type="text" />  
+        </label>
+        <label>Enter pet photo:
+            <input type="file" id="avatar" name="avatar" accept="image/png, image/jpeg"/>  
+        </label>
+        <input type="submit" />
+    </form>
+            
     );
 }
+
 export default ReportFoundPet;
